73372b2e6212825d8983a121eee80c627c65ceeb
root <lovro.markovic@fer.hr>
2025-01-20 13:23:35 +0000

    [scout_bag] Add gps to configuration

---
+++ startup/scout_bag/publish_home.sh
@@ -0,0 +1,23 @@
+ros2 topic pub -r 1 /debug/mavros/global_position/home mavros_msgs/msg/HomePosition "header:
+  stamp:
+    sec: 0
+    nanosec: 0
+  frame_id: ''
+geo:
+  latitude: 45.8131964
+  longitude: 16.0392125
+  altitude: 167.2189432965958
+position:
+  x: 0.0
+  y: 0.0
+  z: 0.0
+orientation:
+  x: 0.0
+  y: 0.0
+  z: 0.0
+  w: 1.0
+approach:
+  x: 0.0
+  y: 0.0
+  z: 0.0" 
+
