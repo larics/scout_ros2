97241c2b96a1d61b80fde91bcb57036bba3ed624
Marko Kozlik <marko.kozlik@fer.unizg.hr>
2025-03-26 11:36:38 +0100

    Update docker build

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -4,21 +4,15 @@ DOCKERFILE=Dockerfile
 IMAGE_NAME=scout_ros2_test
 
 distro="humble"
-ros_distro="humble"
 build_args=""
 for (( i=1; i<=$#; i++));
 do
   param="${!i}"
   echo $param
 
-  if [ "$param" == "--bionic" ]; then
-    distro="bionic"
-    ros_distro="melodic"
-  fi
-
-  if [ "$param" == "--focal" ]; then
-    distro="focal"
-    ros_distro="noetic"
+  if [ "$param" == "--distro" ]; then
+    j=$((i+1))
+    distro="${!j}"
   fi
 
   if [ "$param" == "--dockerfile" ]; then
@@ -38,15 +32,13 @@ do
 
 done
 
-echo "Building $IMAGE_NAME image for $distro / $ros_distro with additional docker arguments $build_args."
+echo "Building $IMAGE_NAME image for $distro with additional docker arguments $build_args."
 echo "Dockerfile: $DOCKERFILE"
 
 # export BUILDKIT_PROGRESS=plain
 export DOCKER_BUILDKIT=1
 docker build \
     $build_args \
-    --build-arg ROS_DISTRO=$ros_distro \
-    --build-arg DISTRO=$distro \
     --build-arg ROS_HOSTNAME=$ROS_HOSTNAME \
     --build-arg ROS_MASTER_URI=$ROS_MASTER_URI \
     --build-arg ROS_IP=$ROS_IP \
