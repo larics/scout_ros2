5178c9fd85528c16f69668b375b46656e04a3e02
Marko Kozlik <marko.kozlik@fer.unizg.hr>
2025-03-26 13:44:12 +0100

    Mount probotica workspace in docker image

--- docker/docker_first_run.sh
+++ docker/docker_first_run.sh
@@ -4,7 +4,7 @@ CONTAINER_NAME=$1
 [ -z "$CONTAINER_NAME" ] && CONTAINER_NAME=scout_ros2_test
 
 IMAGE_NAME=$2
-[ -z "$IMAGE_NAME" ] && IMAGE_NAME=scout_ros2_test:humble
+[ -z "$IMAGE_NAME" ] && IMAGE_NAME=scout_ros2_test:jazzy
 
 # Hook to the current SSH_AUTH_LOCK - since it changes
 # https://www.talkingquickly.co.uk/2021/01/tmux-ssh-agent-forwarding-vs-code/
@@ -42,6 +42,7 @@ docker run \
   --volume /tmp/.x11-unix:/tmp/.x11-unix \
   --volume $HOME/probotika/bags:/root/bags \
   --volume ~/.ssh/ssh_auth_sock:/ssh-agent \
+  --volume /probotica/install:/root/probotica_ws/install \
   --env SSH_AUTH_SOCK=/ssh-agent \
   --env DISPLAY=$DISPLAY \
   --env QT_X11_NO_MITSHM=1 \
