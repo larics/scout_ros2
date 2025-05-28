8b67ca77bd5b869c4666299cf63510a38a7009f6
Marko Kozlik <marko.kozlik@fer.unizg.hr>
2025-05-28 07:50:44 +0000

    [mariner_rw] Update configuration - Punat

---
+++ startup/scout_bag/publish_home_punat.sh
@@ -0,0 +1,23 @@
+ros2 topic pub -r 1 /scout/mavros/home_position/home mavros_msgs/msg/HomePosition "header:
+  stamp:
+    sec: 0
+    nanosec: 0
+  frame_id: ''
+geo:
+  latitude: 45.0284631977722256
+  longitude: 14.6280275887565310
+  altitude: 169.88895940447213
+position:
+  x: 0.0
+  y: 0.0
+  z: 0.0
+orientation:
+  x: 0.0
+  y: 0.0
+  z: 0.0
+  w: 1.0
+approach:
+  x: 0.0
+  y: 0.0
+  z: 0.0" 
+
