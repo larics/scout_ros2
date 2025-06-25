bde1cbcd624689d599315c595e4891d72026efc0
Marko Kozlik <marko.kozlik@fer.unizg.hr>
2025-06-25 08:44:03 +0000

    [marinero_rw] Add dock and people detection, fix home position

--- startup/scout_bag/publish_home_punat.sh
+++ startup/scout_bag/publish_home_punat.sh
@@ -4,9 +4,9 @@ ros2 topic pub -r 1 /scout/mavros/home_position/home mavros_msgs/msg/HomePositio
     nanosec: 0
   frame_id: ''
 geo:
-  latitude: 45.0284631977722256
-  longitude: 14.6280275887565310
-  altitude: 169.88895940447213
+  latitude: 45.0284662 #45.0284827 #45.0284631977722256
+  longitude: 14.6280375 #14.628044 #14.6280275887565310
+  altitude: 54.16162187506501 #169.88895940447213
 position:
   x: 0.0
   y: 0.0
